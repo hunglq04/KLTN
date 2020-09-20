cea9fe10c516b9e2ef9cad210ff92a113e541172
Hung <35440370+hunglq04@users.noreply.github.com>
2020-09-20 14:49:40 +0700

    Rename project folder

--- SMS-FE/src/app/app.module.ts
+++ SMS-FE/src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginAdminComponent } from './login-admin/login-admin.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {RippleModule} from 'primeng/ripple';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginAdminComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+		CheckboxModule,
+		ButtonModule,
+		RadioButtonModule,
+    RippleModule,
+    FormsModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ SMS-FE/src/app/login-admin/login-admin.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-admin',
+  templateUrl: './login-admin.component.html',
+  styleUrls: ['./login-admin.component.css']
+})
+export class LoginAdminComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
